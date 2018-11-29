06c83c5b10de34badd90e022f9219c5013bc417a
Vignesh Balasundaram <vigneshbalasundaram@gmail.com>
2018-11-29 14:34:17 -0800

    Initialize express server with boilerplate routes

---
+++ server/app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const path = require('path');
+const parser = require('body-parser');
+const morgan = require('morgan');
+
+const app = express();
+
+app.use(parser.json());
+app.use(morgan('dev'));
+
+app.use(express.static(path.join(__dirname, './../client/public')));
+
+module.exports = app;
